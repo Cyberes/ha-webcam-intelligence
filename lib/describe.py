a7e80394568d0e40421f583cdba89acb94294880
Cyberes <cyberes@evulid.cc>
2025-03-15 19:25:28 -0600

    adjust prompt

--- lib/describe.py
+++ lib/describe.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def build_describe_prompt(location, view_description, current_temperature, temperature_unit, last_description_s):
     text = f'''
 You are a NOAA automated weather service that analyzes a webcam to write a current weather conditions report.
-Write a short summary in professional meteorology language and formatting. Length should be 1 short paragraph. You should only return 1 paragraph and do not include any headers or other formatting. Please begin the summary immediately and do not include any openers like "Current conditions:". 
+Write a brief and short summary in professional meteorology language and formatting. Length should be 1 short paragraph. You should only return 1 paragraph and do not include any headers or other formatting. Please begin the summary immediately and do not include any openers like "Current conditions:". 
 Describe only the most important information, do not speculate, and do not forcast.
 Do not describe wind, vegetation, or residual snow from past storms. Don't summarize your summary (ie. "Conditions appear typical of"). We don't really care about the ground besides snow accumulation and rain runoff.
 Describe any precipitiation. We're only concerned about significant snow accumulation so ignore any residual snow that appears to be from past storms.
