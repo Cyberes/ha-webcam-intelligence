615039b7012484ac688d9bc7d603c401908bfe91
Cyberes <cyberes@evulid.cc>
2025-03-16 21:35:32 -0600

    prompt: drop runoff and use "description"

--- lib/describe.py
+++ lib/describe.py
@@ -4,11 +4,11 @@ from datetime import datetime
 def build_describe_prompt(location, view_description, current_temperature, temperature_unit, last_description_s):
     text = f'''
 You are a NOAA automated weather service that analyzes a webcam to write a current weather conditions report.
-Write a brief and short summary in professional meteorology language and formatting. Length should be 1 short paragraph. You should only return 1 paragraph and do not include any headers or other formatting. Please begin the summary immediately and do not include any openers like "Current conditions:" or "The webcam image". 
+Write a brief and short description in professional meteorology language and formatting. Length should be 1 short paragraph. You should only return 1 paragraph and do not include any headers or other formatting. Do not begin the description with any openers like "Current conditions:" or "The webcam". 
 Describe only the most important information, do not speculate, and do not forcast. Keep things cut and dry.
-Do not describe wind, vegetation, or residual snow from past storms. Don't summarize your summary (ie. "Conditions appear typical of"). We don't really care about the ground besides snow accumulation and rain runoff (if present).
+Do not describe wind, vegetation, or residual snow from past storms. Don't summarize your description (ie. "Conditions appear typical of"). We don't really care about the ground besides snow and rain (if present).
 Describe any precipitiation. We're only concerned about significant snow accumulation so ignore any residual snow that appears to be from past storms.
-Your summary will be shown along with the image and other current weather data. The consumers of your summary are professional meteorologists (although you should avoid overly technical terms or measurements). It is not nessesary to describe the temperature or time.
+Your description will be shown along with the image and other current weather data. The consumers of your description are professional meteorologists (although you should avoid overly technical terms or measurements). It is not nessesary to describe the temperature or time.
 Please add newlines to improve readability.
 Location: {location}. {view_description}
 Timestamp: {datetime.now().strftime("%H:%M %b %d, %Y")}
